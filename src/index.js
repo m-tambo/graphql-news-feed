d48b1d78bd34c8c972db5d9e71dc9c26e7958869
Michael Tambornino <michael.tambornino@gmail.com>
2017-10-02 22:51:36 -0500

    New component for login

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import './styles/index.css'
+// 1
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
+
+// 2
+// use the command 'graphcool endpoints' in the console
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj86ppp51061b0171zhnn00px'
+})
+
+// 3
+const client = new ApolloClient({
+  networkInterface
+})
+
+// 4
+ReactDOM.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
+  , document.getElementById('root')
+)
+registerServiceWorker()
