4d9f7ac5682e1db30d6a9eee0d5545a8239235a8
Michael Tambornino <michael.tambornino@gmail.com>
2017-10-03 18:42:34 -0500

    Add upvoting functionality

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -4,6 +4,16 @@ import { gql, graphql } from 'react-apollo';
 
 class LinkList extends Component {
 
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+
+    const data = store.readQuery({ query: ALL_LINKS_QUERY })
+
+    const votedLink = data.allLinks.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+
+    store.writeQuery({ query: ALL_LINKS_QUERY, data })
+  }
+
   render() {
 
     if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
@@ -18,8 +28,8 @@ class LinkList extends Component {
 
     return (
       <div>
-        {linksToRender.map(link => (
-          <Link key={link.id} link={link}/>
+        {linksToRender.map((link, index) => (
+          <Link key={link.id} updateStoreAfterVote={this._updateCacheAfterVote}  index={index} link={link}/>
         ))}
       </div>
     )
@@ -27,18 +37,25 @@ class LinkList extends Component {
 
 }
 
-const ALL_LINKS_QUERY = gql`
-
-  query allLinksQuery {
+export const ALL_LINKS_QUERY = gql`
+  query AllLinksQuery {
     allLinks {
       id
       createdAt
       url
       description
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
     }
   }
-
-
 `
 
 export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' }) (LinkList)
