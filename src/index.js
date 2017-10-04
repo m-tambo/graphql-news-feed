ba83a4e0294324a6ab62bcd243aafb255ab15756
Michael Tambornino <michael.tambornino@gmail.com>
2017-10-04 16:57:27 -0500

    Add websocket subscriptions for realtime updating

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import './styles/index.css'
@@ -9,8 +10,20 @@ import { GC_AUTH_TOKEN } from './constants'
 
 // use the command 'graphcool endpoints' in the console
 const networkInterface = createNetworkInterface({
-  uri: 'https://api.graph.cool/simple/v1/cj86ppp51061b0171zhnn00px'
+  uri: 'https://api.graph.cool/simple/v1/<project-id>'
 })
+
+const wsClient = new SubscriptionClient('wss://subscriptions.us-west-2.graph.cool/v1/cj86ppp51061b0171zhnn00px', {
+  reconnect: true,
+  connectionParams: {
+     authToken: localStorage.getItem(GC_AUTH_TOKEN),
+  }
+})
+
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient
+)
 
 networkInterface.use([{
   applyMiddleware(req, next) {
@@ -24,7 +37,7 @@ networkInterface.use([{
 }])
 
 const client = new ApolloClient({
-  networkInterface
+  networkInterface: networkInterfaceWithSubscriptions
 })
 
 ReactDOM.render(
