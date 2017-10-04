4d22e9c224efdb99e3e8ae9bd59ab1a746e8727c
Michael Tambornino <michael.tambornino@gmail.com>
2017-10-04 15:58:36 -0500

    Add filterning search capability

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,9 @@ class Header extends Component {
       <div className='flex pa1 justify-between nowrap orange'>
         <div className='flex flex-fixed black'>
           <div className='fw7 mr1'>Hacker News</div>
-          <Link to='/' className='ml1 no-underline black'>new</Link>
+          <Link to='/' className='ml1 no-underline black'>news</Link>
+          <div className='ml1'>|</div>
+          <Link to='/search' className='ml1 no-underline black'>search</Link>
           {userId &&
           <div className='flex'>
             <div className='ml1'>|</div>
